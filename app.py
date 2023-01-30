c65a84fb1431efda918783f446056cdb0650a49c
UlisesJE <ujuareze1700@alumno.ipn.mx>
2023-01-29 19:51:54 -0600

    preparando entorno para tailwindcss v2

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask,redirect,url_for,render_template,request
 
 app=Flask(__name__)
-
 @app.route('/',methods=['GET','POST'])
 def home():
-
     return render_template('index.html')
 
 if __name__ == '__main__':
