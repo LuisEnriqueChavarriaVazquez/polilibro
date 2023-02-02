0ed7e0ce974f348a457fdc834c9d6e0a27114f4f
luis <luisechvz@gmail.com>
2023-02-02 17:06:18 -0600

    Primer ejemplo de unidades

---
+++ static/js/menuLateralUnidad1RememberState.js
@@ -0,0 +1,21 @@
+// let rememberStateUnit1 = document.getElementsByClassName('rememberStateUnit1');
+// let arrayOptions = [...rememberStateUnit1];
+
+// let numeroElemento = "";
+// if(localStorage.getItem('viewed_unit1') == null){
+//     localStorage.setItem('viewed_unit1', 'empty')
+// }
+
+// let opcionMenuStoreUnit1 = localStorage.setItem('opcionMenuStoreUnit1', "1")
+// arrayOptions.forEach((element) => {
+//     element.addEventListener('click', () => {
+//         //Obtenemos el valor del contador
+//         numeroElemento += element.getAttribute('contador') + ",";
+//         localStorage.setItem('viewed_unit1', numeroElemento)
+
+//         element.classList.add('leftBorderOn');
+//         element.classList.remove('leftBorderSecondary');
+//         element.classList.remove('leftBorder');
+
+//     })
+// })
