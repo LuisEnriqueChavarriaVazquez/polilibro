1b66d9361bcdf8c0a5d79feff98785f6d6791dea
luis <luisechvz@gmail.com>
2023-02-03 17:23:28 -0600

    Manual adelantado

--- static/js/activateComponentsMaterialize.js
+++ static/js/activateComponentsMaterialize.js
@@ -2,4 +2,6 @@ $(document).ready(function () {
     $('.sidenav').sidenav();
     $('.dropdown-trigger').dropdown();
     $('.tabs').tabs();
+    $('.scrollspy').scrollSpy();
+    $('.materialboxed').materialbox();
 });
